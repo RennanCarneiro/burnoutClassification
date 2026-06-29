5bb2256c0e59410f39e2f970ffa8f7e6fbf35fd5
Rennan <rennanbarros10@gmail.com>
2026-06-28 22:49:38 -0300

    feat: Add Random Forest model implementation for predicting Burn Rate
    - Introduced a new script `randomForest.py` for training a Random Forest classifier.
    - Loaded and preprocessed training data from `train_cleaned.csv`.
    - Mapped categorical variables to numerical values for model compatibility.
    - Binned the 'Burn Rate' into discrete categories.
    - Split the dataset into training and testing sets.
    - Fitted the Random Forest model using the training data.

---
+++ model/randomForest.py
@@ -0,0 +1,38 @@
+#%%
+import pandas as pd
+
+df = pd.read_csv("../data/processed/train_cleaned.csv")
+df.head()
+# %%
+mapeamento_gender = {'Female':1, 'Male':0}
+mapeamento_company = {'Service':1, 'Product':0}
+mapeamento_WFH = {'Yes':1, 'No':0}
+
+df['Gender'] = df['Gender'].map(mapeamento_gender)
+df['Company Type'] = df['Company Type'].map(mapeamento_company)
+df['WFH Setup Available'] = df['WFH Setup Available'].map(mapeamento_WFH)
+
+bins = [0.0, 0.31, 0.45, 0.59, 1.0]
+labels = [0,1,2,3]
+
+df['Burn Rate'] = pd.cut(df['Burn Rate'], bins=bins, labels=labels, include_lowest=True)
+
+target = 'Burn Rate'
+features = ['Gender', 'Company Type', 'WFH Setup Available', 'Designation', 
+              'Resource Allocation', 'Mental Fatigue Score']
+
+X, y = df[features], df[target]
+y
+# %%
+y.isnull().sum()
+# %%
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42)
+# %%
+from sklearn.ensemble import RandomForestClassifier
+
+modelo = RandomForestClassifier(random_state=42)
+
+modelo.fit(X_train, y_train)
+# %%
